3fa02869d4c9d031ed93ab5c4f05139524296426
Sudarshan Verma <132141458+sudarshanverma19@users.noreply.github.com>
2024-11-01 16:46:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+const todoList = document.getElementById("todoList");
+const newTodoInput = document.getElementById("newToDoInput");
+const newTodobtn= document.getElementById("addTodoBtn");
+
+addTodoBtn.addEventListener("click",()=>{
+    const newTodoText = newTodoInput.value;
+    if (newTodoText!=="") {
+        const newTodoItem = document.createElement("li");
+        newTodoItem.innerText = newTodoText;
+        const deleteTodoBtn = document.createElement("button");
+        deleteTodoBtn.innerText = "X" ;
+        deleteTodoBtn.classList.add("delete-todo-btn");
+        deleteTodoBtn.addEventListener("click",function(){
+            newTodoItem.remove();
+        });
+        newTodoItem.appendChild(deleteTodoBtn);
+        todoList.appendChild(newTodoItem);
+        newTodoInput.value = "" ;
+    }
+})
